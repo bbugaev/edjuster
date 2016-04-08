997fe8ecd5f7c499c44a8b0c8c173a84558b337f
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2016-04-08 23:26:28 +0400

    Image reading

--- edjuster/edjuster.py
+++ edjuster/edjuster.py
@@ -1,13 +1,21 @@
 #! /usr/bin/env python2
 
+import os.path as path
+
 import click
+from scipy.ndimage import imread
 
 
 @click.command()
 @click.argument('input_folder', type=click.Path(exists=True, file_okay=False))
 def edjust(input_folder):
     """Adjusts pose of 3D object"""
-    click.echo('INPUT_FOLDER: %s' % input_folder)
+
+    image_path = path.join(input_folder, 'image.bmp')
+    try:
+        image = imread(image_path)
+    except IOError:
+        raise click.FileError(image_path, '3D object image')
 
 
 if __name__ == '__main__':
