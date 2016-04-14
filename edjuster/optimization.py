f129db37ec290e019f4adad7b1e4d12d6a5f992b
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2016-04-15 00:17:19 +0400

    Gradient calculation

--- edjuster/optimization.py
+++ edjuster/optimization.py
@@ -1,6 +1,8 @@
 from itertools import izip
 
 import numpy as np
+from scipy.ndimage import convolve
+from scipy.interpolate import RectBivariateSpline
 
 
 def select_points(mesh_edges, point_count):
@@ -26,3 +28,31 @@ def select_points(mesh_edges, point_count):
         prefix_length += length
 
     return np.array(points[:point_count])
+
+
+class Gradient(object):
+    SCHARR_KERNEL_X = np.array([[3, 0, -3], [10, 0, -10], [3, 0, -3]]) / 32.0
+    SCHARR_KERNEL_Y = np.array([[3, 10, 3], [0, 0, 0], [-3, -10, -3]]) / 32.0
+
+    def __init__(self, image):
+        image = np.flipud(image)
+        x_indices = np.arange(image.shape[1])
+        y_indices = np.arange(image.shape[0])
+        x_deriv = convolve(image, Gradient.SCHARR_KERNEL_X)
+        y_deriv = convolve(image, Gradient.SCHARR_KERNEL_Y)
+        self._x_deriv = RectBivariateSpline(y_indices, x_indices, x_deriv)
+        self._y_deriv = RectBivariateSpline(y_indices, x_indices, y_deriv)
+
+    def __call__(self, coordinates):
+        if coordinates.ndim > 1:
+            coordinates = np.hsplit(coordinates[:, ::-1], 2)
+        else:
+            coordinates = coordinates[::-1]
+        return np.hstack((
+            self._x_deriv(*coordinates, grid=False),
+            self._y_deriv(*coordinates, grid=False)
+        ))
+
+
+def approximate_edge_integral(image, mesh_edges, point_count):
+    pass
